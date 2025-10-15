39eb1dadbcbad0ec680bd7a39eef075a02643d78
Gabriel <mygodsent002@outlook.com>
2025-10-15 13:16:42 +0100

    Added a vpc module

--- main.tf
+++ main.tf
@@ -29,12 +29,3 @@ module "vpc" {
   availability_zone  = var.availability_zone
 }
 
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
-}
-output "private_subnet_id" {
-  value = module.vpc.private_subnet_id
-}
